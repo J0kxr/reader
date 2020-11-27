3a6c0658e10a6dfb6d168e781e1df9ce140e4891
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2020-11-27 14:10:52 +0100

    Added Station Class to create Station Objects

---
+++ src/main/java/reader/Station.java
@@ -0,0 +1,12 @@
+public class Station{
+
+    private String stationName;//Name of Gasstation ex. Shell
+    private float pirce;
+    private String adress;
+    private String openHrs;
+
+    public Station(){
+
+
+    }
+}
