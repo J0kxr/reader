328d127fbd2fe24716fbefa0d3023c8bafaf1bd5
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2020-12-11 14:17:12 +0100

    removed try/catch-block

--- src/main/java/reader/FileIntoDatabase.java
+++ src/main/java/reader/FileIntoDatabase.java
@@ -16,11 +16,11 @@ public class FileIntoDatabase{
     */
     //get hole bunch of datanames in a string and substring it to one dataname after another 
 
-    public String DateiAuslesen(String dateiname){
+    public String DateiAuslesen(String dateiname) throws IOException{
         BufferedReader file = null;
         String fullText = null;
 
-        try{
+        
         file = new BufferedReader(new FileReader(dateiname));
         //readout the file
         String zeile = null;
@@ -28,22 +28,9 @@ public class FileIntoDatabase{
         while((zeile = file.readLine()) != null){//readLine read out the line
                 fullText = fullText + zeile;
             }
-        return fullText;
-
-        }catch (IOException e){
-            e.printStackTrace();
-
-        }finally{
-             try{
-                if (file != null){
-                    file.close();
-                }
-            }catch (IOException e){
-            e.printStackTrace();
-            }
+        file.close();
+        return fullText;       
         }
-        return "Datei nicht auslesesbar!";
-    }
 
     public String getStationName(String html){
 
