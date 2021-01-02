b88c91ca0149aed2691511ba81e6555fd94443df
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2021-01-02 02:14:51 +0100

    changed html into static variable for more general access

--- src/main/java/reader/read.java
+++ src/main/java/reader/read.java
@@ -3,33 +3,42 @@ package reader;
 import java.io.*;
 
 public class read{
-    public read(){
-            
-        String htmlString = null;
-            
-        public String readHTML(String path) throws IOException{
-        
+
+    private static String html;
+
+        public void readHTML(String path) throws IOException{
+            String text = null;
         
             BufferedReader reader = new BufferedReader(new FileReader("path"));
         
             while(null != reader.readLine()){
-                htmlString += reader.readLine();
+                text += reader.readLine();
             }
         
             reader.close();
-            return htmlString;
+            setHTML(text);
         }
-        public String parseGasStation(String html){
+
+    public read(int ID, String path){
+        readHTML(path);
+        parseGasStation(html, ID);
+        parsePrice(html, ID);
+    }
+
+        public String parseGasStationOverID(String html, int ID){
             //liest Tankstelle aus
-            return html.substirng
+            return html.substring(4, 6);
         }
-        
-        public int GasToID(String GasStation){//setzt die ID fuer die jeweilige Tankstelle
-        
-        }
-        public float parsePrice(String html){
+
+        public float parsePrice(String html, int ID){
         
         }//liest Preise aus
 
-    
+        public void setHTML(String hmtl){
+            this.html = html;
+        }
+
+        public String getHMTL(){
+            return this.html;
+        }
 }
