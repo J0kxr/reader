22f31eba10834b1ec2698ae781003272a02c066f
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2021-01-02 02:29:22 +0100

    constructed the first framwork for becoming GasStation-name(over ID)

--- src/main/java/reader/read.java
+++ src/main/java/reader/read.java
@@ -6,39 +6,66 @@ public class read{
 
     private static String html;
 
-        public void readHTML(String path) throws IOException{
-            String text = null;
+        public void readHTML(String path) throws FileNotFoundException, IOException{
+            String lines = null;
         
             BufferedReader reader = new BufferedReader(new FileReader("path"));
         
             while(null != reader.readLine()){
-                text += reader.readLine();
+                lines += reader.readLine();
             }
         
             reader.close();
-            setHTML(text);
+            setHTML(lines);
         }
 
-    public read(int ID, String path){
+    public read(int ID, String path) throws IOException{
         readHTML(path);
-        parseGasStation(html, ID);
-        parsePrice(html, ID);
+        parseGasStationOverID(ID);
+        parsePrice(ID);
     }
 
-        public String parseGasStationOverID(String html, int ID){
+        public String parseGasStationOverID(int ID){
             //liest Tankstelle aus
+            int i = ID;
+            String GasStation;
+            String text = getHTML();
+
+            switch(i){
+                case 00:
+                    GasStation = text.substring();//Shell
+                    break;
+                case 01:
+                    GasStation = text.substring();//Freie 
+                    break;
+                case 02:
+                    GasStation = text.substring();//Edeka
+                    break;
+                case 03:
+                    GasStation = text.substring();//OMV
+                    break;
+                default 04:
+                    GasStation = text.substring();//Mowak aka Tankpool
+                    break;
+
+            }
+
+
             return html.substring(4, 6);
         }
 
-        public float parsePrice(String html, int ID){
+        public float parsePrice(int ID){
+            //ließt Preis aus 
+            
         
-        }//liest Preise aus
+        }
+
 
         public void setHTML(String hmtl){
             this.html = html;
         }
 
-        public String getHMTL(){
+        public String getHTML(){
             return this.html;
         }
 }
