90a02079d82a7d3d5b3359b344990b3d43a6000b
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2021-01-02 02:02:22 +0100

    added constructor for Gas Station identification about IDs

--- src/main/java/reader/read.java
+++ src/main/java/reader/read.java
@@ -3,26 +3,33 @@ package reader;
 import java.io.*;
 
 public class read{
-
-    public String readHTML(String path) throws IOException{
+    public read(){
+            
         String htmlString = null;
-
-        BufferedReader reader = new BufferedReader(new FileReader("path"));
-
-        while(null != reader.readLine()){
-            htmlString += reader.readLine();
-        }
-    
-        reader.close();
-        return htmlString;
-    }
-
-    public float parsePrice(){}
-
-    public String parseGasStation(){
-    }
-
-    public int GasToID(String GasStation){
+            
+        public String readHTML(String path) throws IOException{
         
-    }
+        
+            BufferedReader reader = new BufferedReader(new FileReader("path"));
+        
+            while(null != reader.readLine()){
+                htmlString += reader.readLine();
+            }
+        
+            reader.close();
+            return htmlString;
+        }
+        public String parseGasStation(String html){
+            //liest Tankstelle aus
+            return html.substirng
+        }
+        
+        public int GasToID(String GasStation){//setzt die ID fuer die jeweilige Tankstelle
+        
+        }
+        public float parsePrice(String html){
+        
+        }//liest Preise aus
+
+    
 }
