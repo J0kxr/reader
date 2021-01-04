71ab5d29950c026268a7abe0d11eeeec592a248a
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2021-01-05 00:55:55 +0100

    seperated unused calsses

---
+++ src/main/java/reader/unused/FileIntoDatabase.java
@@ -0,0 +1,59 @@
+/*
+package reader;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileIntoDatabase{
+
+    private String alleDateiNamen = null;
+
+    /*
+    public String dateinamenAusOrdner(String s){
+        String dataname;
+        dataname = s.substring(#1begin, #2end);
+        return dataname;
+    }
+    
+    
+    //get hole bunch of datanames in a string and substring it to one dataname after another 
+
+    public String DateiAuslesen(String dateiname) throws IOException{
+        BufferedReader file = null;
+        String fullText = null;
+
+        
+        file = new BufferedReader(new FileReader(dateiname));
+        //readout the file
+        String zeile = null;
+
+        while((zeile = file.readLine()) != null){//readLine read out the line
+                fullText = fullText + zeile;
+            }
+        file.close();
+        return fullText;       
+        }
+
+    public String getStationName(String html){
+
+        String stationName;
+        stationName = html.substring(8968, )
+    } 
+
+    public String getPrices(String hmtl){ // sets the price attribte of eevery station class
+        Station [] stationNumber= new Station();
+
+        float[] price = new float[0, 1, 2, 3];
+        price[0] = (float) html.substring(9042, 9047);
+        price[1] = (float) html.substring(10188, 10193);
+        price[2] = (float) html.substring(11331, 11336);
+        price[3] = (float) html.substring(12485, 12490);
+    }
+
+    public String getAlleDateiNamen(){
+        return this.alleDateiNamen;
+    }
+
+}
+
+*/
---
+++ src/main/java/reader/unused/Station.java
@@ -0,0 +1,18 @@
+/*
+public class Station{
+
+    private String stationName;//Name of Gasstation ex. Shell
+    private float pirce;
+    private String adress;
+    private String openHrs;
+
+    public Station(){
+
+
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+}
+*/
